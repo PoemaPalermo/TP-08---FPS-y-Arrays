fd2b98ceff4a7146db9971af5bf73ed36f911be1
PoemaPalermo <163904623+PoemaPalermo@users.noreply.github.com>
2024-06-26 15:31:06 -0300

    Finished

---
+++ Assets/CharacterScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int hambre;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Comer(int valorAlimentario)
+    {
+        hambre -= valorAlimentario;
+        if (hambre <= 0)
+        {
+            hambre = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        AlimentoScript alimento;
+        alimento = other.GetComponent<AlimentoScript>();
+        if (alimento)
+        {
+            Comer(alimento.valor);
+            Destroy(other.gameObject);
+        }
+    }
+}
